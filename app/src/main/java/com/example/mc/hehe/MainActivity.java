ef0ac8eae6dc4def0d639313bf0b9cc0e7c1f653
mochao1 <605835780@qq.com>
2018-06-05 14:31:22 +0800

    第七次提交

--- app/src/main/java/com/example/mc/hehe/MainActivity.java
+++ app/src/main/java/com/example/mc/hehe/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    //呵呵哈哈嗷嗷奥奥喵喵啊啊
+    //呵呵哈哈嗷嗷奥奥喵喵啊啊哦哦
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
